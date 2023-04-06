c7ee56e5c2ffcc7d4d1309c7ae8ab226ae8925e7
giovanastefan <89399240+giovanastefan@users.noreply.github.com>
2023-04-06 18:56:32 -0300

    Update InsercaoTabelas.sql

--- InsercaoTabelas.sql
+++ InsercaoTabelas.sql
@@ -10,7 +10,6 @@ INSERT INTO categoria (categoria) VALUES
 ('Pizzaria'),
 ('Hamburgueria');
 
-/* Inserindo os endereços */
 INSERT INTO endereco (rua, numero, cidade, cep, estado, pais) VALUES
 ('Rua A', 123, 'São Paulo', '01234-567', 'São Paulo', 'Brasil'),
 ('Rua B', 456, 'Rio de Janeiro', '98765-432', 'Rio de Janeiro', 'Brasil'),
@@ -23,11 +22,20 @@ INSERT INTO endereco (rua, numero, cidade, cep, estado, pais) VALUES
 ('Rua I', 864, 'Natal', '65432-109', 'Rio Grande do Norte', 'Brasil'),
 ('Rua J', 279, 'Brasília', '10987-654', 'Distrito Federal', 'Brasil');
 
-/* Inserindo os estabelecimentos */
 INSERT INTO estabelecimento (id_endereco, nome, id_categoria, media_nota, descricao, site, imagem, data_abertura, hora_funcionamento, menu_url, tipo_pagamento) VALUES
 (1, 'Restaurante do Japa', 1, 0, 'Restaurante japonês com opções de sushi, sashimi e pratos quentes.', 'www.restaurantedojapa.com.br', 'www.restaurantedojapa.com.br/menu', '2020-01-01', '11:00 - 23:00', 'menujapa.pdf', 'Cartão de crédito, débito e dinheiro'),
 (2, 'Pasta & Vino', 2, 0, 'Restaurante italiano com massas artesanais e carta de vinhos selecionados.', 'www.pastaevino.com.br', 'www.pastaevino.com.br/menu', '2019-05-15', '12:00 - 22:00', 'menupastaevino.pdf', 'Cartão de crédito e débito'),
 (3, 'Bar do Mineiro', 3, 0, 'Restaurante de comida brasileira com opções de pratos típicos mineiros.', 'www.bardomineiro.com.br', 'www.bardomineiro.com.br/menu', '2018-07-30', '11:30 - 00:00', 'menubardomineiro.pdf', 'Cartão de crédito, débito e dinheiro');
 
+INSERT INTO contato (email, telefone, id_estabelecimento) VALUES
+('contato@restaurantedojapa.com.br', '(11) 5555-5555', 1),
+('contato@pastaevino.com.br', '(11) 4444-4444', 2),
+('contato@bardomineiro.com.br', '(11) 3333-3333', 3);
+
+INSERT INTO usuario (nome, id_categoria) VALUES ('Paulo Santos', 1);
+
+INSERT INTO login (usuario_id, email, senha) VALUES (1, 'paulo_santos@gmail.com', 'senha123');
+
+INSERT INTO donoEstabelecimento (id_usuario, id_estabelecimento) VALUES (1, 1);
 
 
