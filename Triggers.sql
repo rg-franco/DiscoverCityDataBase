add05c3616fc5321eab81108cfbec74ac3610001
tiagolima95 <96542145+tiagolima95@users.noreply.github.com>
2023-04-23 23:00:22 -0300

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -12,3 +12,16 @@ END;
 |
 DELIMITER ;
 
+
+DROP TRIGGER IF EXISTS data_avaliacao_atual;
+DELIMITER |
+CREATE TRIGGER data_avaliacao_atual BEFORE INSERT ON db_discoverycity.avaliacao
+FOR EACH ROW
+BEGIN 
+	SET new.data_avaliacao = curdate();
+END;
+|
+DELIMITER ;
+		
+
+
