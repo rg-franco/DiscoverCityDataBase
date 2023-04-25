e48ae1e26fc6bb83b0594caca09e7c03f6f28ee9
giovanastefan <89399240+giovanastefan@users.noreply.github.com>
2023-04-25 19:26:32 -0300

    Update cupomValido.sql

--- Cursor/cupomValido.sql
+++ Cursor/cupomValido.sql
@@ -1,22 +1,25 @@
-drop procedure if exists cupomValido;
+DROP PROCEDURE IF EXISTS cupomValido;
 SET SQL_SAFE_UPDATES = 0;
-delimiter ||
-create procedure cupomValido()
-begin
-    declare status_consulta date;
-	DECLARE cur CURSOR FOR SELECT data_validade FROM cupons;
+DELIMITER ||
+CREATE PROCEDURE cupomValido()
+BEGIN
+    DECLARE status_consulta BOOLEAN;
+    DECLARE id_v INT;
+    DECLARE data_v DATE;
+    DECLARE hoje date;
+    
+    DECLARE cur CURSOR FOR SELECT id, data_validade FROM cupons;
+    
+    
+    OPEN cur;
+    start_loop: LOOP
+        FETCH NEXT FROM cur INTO id_v, data_v;
+        if data_v < curdate() then
+			UPDATE cupons SET valido = false where id = id_v;
+		end if;
+    END LOOP;
+    CLOSE cur;
+END ||
+DELIMITER ;
 
-    open cur;
-		start_loop: loop
-			fetch cur into status_consulta;
-				if status_consulta < curdate() then
-					update cupons set valido = false;
-                end if;
-		end loop;
-    close cur;
-	
-end ||
-
-delimiter ;
-
-call cupomValido();
+CALL cupomValido();
