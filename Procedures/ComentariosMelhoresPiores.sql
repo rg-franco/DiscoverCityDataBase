c0560a1cf17bfb7eac888af435f398f0a17697d7
tiagolima95 <96542145+tiagolima95@users.noreply.github.com>
2023-04-25 20:02:11 -0300

    Add files via upload

---
+++ Procedures/ComentariosMelhoresPiores.sql
@@ -0,0 +1,18 @@
+DELIMITER ||
+
+CREATE PROCEDURE comentariosMelhoresPiores()
+BEGIN
+    SELECT 'Melhores' AS tipo, CONCAT(a1.id_usuario, ': ', a1.comentarios) AS comentario, a1.nota AS nota
+FROM avaliacao a1
+WHERE a1.nota = (SELECT MAX(a2.nota) FROM avaliacao a2 WHERE a2.id_estabelecimento = a1.id_estabelecimento)
+UNION ALL
+SELECT 'Piores' AS tipo, CONCAT(a3.id_usuario, ': ', a3.comentarios) AS comentario, a3.nota AS nota
+FROM avaliacao a3
+WHERE a3.nota = (SELECT MIN(a4.nota) FROM avaliacao a4 WHERE a4.id_estabelecimento = a3.id_estabelecimento)
+ORDER BY tipo, nota DESC;
+
+END ||
+
+DELIMITER ;
+
+CALL comentariosMelhoresPiores();
