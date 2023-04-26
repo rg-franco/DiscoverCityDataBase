2936d120e43b2b831ca0f8c0bda88e7f465c70cc
giovanastefan <89399240+giovanastefan@users.noreply.github.com>
2023-04-26 19:35:16 -0300

    Update lugares_abertos_agora.sql

--- Views/lugares_abertos_agora.sql
+++ Views/lugares_abertos_agora.sql
@@ -1,6 +1,6 @@
 DROP VIEW IF EXISTS lugares_abertos_agora;
 CREATE VIEW lugares_abertos_agora AS
-SELECT est.nome as 'Estabelecimento', est.descricao, CONCAT(en.rua,', ', en.numero,', ', en.bairro,' - ', en.cidade) as 'Endereço', est.inicio_funcionamento, est.fim_funcionamento
+SELECT DISTINCT est.nome as 'Estabelecimento', est.descricao, CONCAT(en.rua,', ', en.numero,', ', en.bairro,' - ', en.cidade) as 'Endereço', est.inicio_funcionamento, est.fim_funcionamento
 FROM estabelecimento est
 JOIN endereco en	ON curtime() > est.inicio_funcionamento AND curtime() < est.fim_funcionamento 
 AND est.id = en.id_estabelecimento;
