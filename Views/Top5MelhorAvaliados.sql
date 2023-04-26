e0228bcf103e4d733ce1158074b08a50ab1a7742
giovanastefan <89399240+giovanastefan@users.noreply.github.com>
2023-04-26 19:31:52 -0300

    Update Top5MelhorAvaliados.sql

--- Views/Top5MelhorAvaliados.sql
+++ Views/Top5MelhorAvaliados.sql
@@ -1,7 +1,6 @@
 drop view if exists top5_melhores_avaliados;
 create view top5_melhores_avaliados as 
-select nome, media_nota, telefone, hora_funcionamento from estabelecimento e
+select nome, media_nota, telefone, inicio_funcionamento, fim_funcionamento from estabelecimento e
 join contato c on c.id_estabelecimento = e.id
-order by media_nota
+order by media_nota DESC
 limit 5;
-
